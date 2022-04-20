ae50524d10735640ff5f2e11ab80a54df475405a
IOrozakunov23 <islamburunduk045>
2022-04-20 22:45:43 +0500

    renew Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,19 @@
 import "./Header.css";
-import headerImage from "../../assets/kyrgyz-dishes.jpg";
+// import headerImage from "../../assets/food.jpg";
 
-function Header() {
-  const style = {
-    backgroundImage: `url(${headerImage})`
-  };
-
+function Header({ image, title, children }) {
   return (
-    <header className="Header" style={style}>
-      <h1>Kyrgyz national dishes</h1>
-      <p>This is an internet shop</p>
+    <header className="Header">
+      <img src={image} />
+      <section>
+        <h1>{title}</h1>
+        <p>{children}</p>
+      </section>
     </header>
   );
 }
-
+{
+  /* <h1>Kyrgyz dishes</h1>
+      <p>This is an internet shop</p> */
+}
 export default Header;
