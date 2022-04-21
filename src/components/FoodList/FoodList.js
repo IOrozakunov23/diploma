6646ddf631393f86c505e765af97a8e99430febb
IOrozakunov23 <islamburunduk045>
2022-04-21 11:59:56 +0500

    upadate

--- src/components/FoodList/FoodList.js
+++ src/components/FoodList/FoodList.js
@@ -2,15 +2,11 @@ import FoodItem from "./FoodItem/FoodItem";
 import classes from "./FoodList.module.css";
 
 function FoodList({ foods }) {
-  const foodItems = Object.keys(foods).map(id => (
+  const foodItems = Object.keys(foods).map((id) => (
     <FoodItem key={id} food={foods[id]} />
   ));
 
-  return (
-    <div className={classes.FoodList}>
-      {foodItems}
-    </div>
-  );
+  return <div className={classes.FoodList}>{foodItems}</div>;
 }
 
 export default FoodList;
