08837fd3ff4f86b115fca62f722ee780458b82e5
IOrozakunov23 <islamburunduk045>
2022-05-11 00:35:38 +0500

    added drawer file

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,20 +1,13 @@
-import Logo from "../ui/Logo/Logo";
-import "./Nav.css";
-
+import classes from "./Nav.css";
 import NavItem from "./NavItem/NavItem";
 
 function Nav() {
   return (
-    <nav className="Nav">
-      <div className="container">
-        <Logo />
-        <ul>
-          <NavItem url="/">Home</NavItem>
-          <NavItem url="/Foods">Foods</NavItem>
-          <NavItem url="/Drinks">Drinks</NavItem>
-        </ul>
-      </div>
-    </nav>
+    <ul className={classes.Nav}>
+      <NavItem url="/">Home</NavItem>
+      <NavItem url="/products">Foods</NavItem>
+      <NavItem url="/contacts">Drinks</NavItem>
+    </ul>
   );
 }
 
