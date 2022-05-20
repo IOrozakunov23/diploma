6b2869dfdf82ac797e7b3ba19a7f240b2341a621
IOrozakunov23 <islamburunduk045>
2022-05-21 00:22:03 +0500

    added drinks

--- src/data/drinks.js
+++ src/data/drinks.js
@@ -1,25 +1,25 @@
-import bismarckImage from "../assets/bismarck.jpg";
-import tartufataImage from "../assets/tartufatata.jpg";
-import francescanaImage from "../assets/francescana.jpg";
+import colsImage from "../assets/cols.jpg";
+import spriteImage from "../assets/sprite.jpg";
+import fantaImage from "../assets/fanta.jpg";
 const products = [
   {
-    image: bismarckImage,
-    productId: "bismarck",
-    title: "Bismarck",
+    image: colsImage,
+    productId: "cola",
+    title: "Cola",
     description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis arcu purus, rhoncus fringilla vestibulum vel, dignissim vel ante. Nulla facilisi. Nullam a urna sit amet tellus pellentesque egestas in in ante.`,
     price: 150,
   },
   {
-    image: tartufataImage,
-    productId: "tartufata",
-    title: "Tartufata",
+    image: spriteImage,
+    productId: "sprite",
+    title: "Sprite",
     description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis arcu purus, rhoncus fringilla vestibulum vel, dignissim vel ante. Nulla facilisi. Nullam a urna sit amet tellus pellentesque egestas in in ante.`,
     price: 200,
   },
   {
-    image: francescanaImage,
-    productId: "francescana",
-    title: "Francescana",
+    image: fantaImage,
+    productId: "fanta",
+    title: "Fanta",
     description: `Osteria Francescana is a restaurant owned and run by chef Massimo Bottura in Modena, Italy. In 2018, William Reed Business Media named Osteria Francescana the best restaurant in the world that year in their annual The World's 50 Best Restaurants. In 2016 and 2018, it was rated as the world's best restaurant in the World's 50 Best Restaurants. It was the first Italian restaurant to earn the award. It was also second best in 2015 and third best in 2013 and 2014.`,
     price: 100,
   },
--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,5 +1,5 @@
 import Header from "../components/Header/Header";
-  import headerImage from "../assets/header.jpg";
+  import headerImage from "../assets/drinks.jpg";
   import DrinksList from "../components/DrinksList/DrinksList";
   import { getProducts } from "../data/drinks";
   
