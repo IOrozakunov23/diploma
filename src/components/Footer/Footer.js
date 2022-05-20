23a62dea1dfb9912460227fab42ae857c51b588a
IOrozakunov23 <islamburunduk045>
2022-05-21 00:23:48 +0500

    change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,6 @@ function Footer() {
   return (
     <footer className="Footer">
       <div className="container">
-      {/* <ul className={Footer}>
-      <a url="/">Home</a>
-      <a url="/foods">Foods</a>
-      <a url="/drinks">Drinks</a>
-    </ul> */}
     <p>The Don Peppe crew first and foremost values an authentic, freshly baked slice of pizza perfection</p>
         </div>
     </footer>
