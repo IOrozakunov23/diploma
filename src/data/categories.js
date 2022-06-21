e9bad2333d2bbfd350dd1e06ab93c2130496c449
IOrozakunov23 <islamburunduk045>
2022-06-21 17:22:36 +0500

    added drink's

--- src/data/categories.js
+++ src/data/categories.js
@@ -3,7 +3,8 @@ import spriteImage from "../assets/spritee.jpg";
 import fantaImage from "../assets/fant.jpg";
 import cocteilsImage from "../assets/drink1.jpg";
 import cocteils1Image from "../assets/drink2.jpg";
-import cocteilsImage2 from "../assets/drink3.jpg";
+import cocteils2Image from "../assets/drink3.jpg";
+import { trackForMutations } from "@reduxjs/toolkit/dist/immutableStateInvariantMiddleware";
 const categories = [
   {
     image: colsImage,
@@ -44,7 +45,7 @@ const categories = [
     {
       image: cocteils2Image,
         productId: "cocteil",
-        title: "Fanta",
+        title: "cocteil",
         description: `Osteria Francescana is a restaurant owned and run by chef Massimo Bottura in Modena, Italy. In 2018, William Reed Business Media named Osteria Francescana the best restaurant in the world that year in their annual The World's 50 Best Restaurants. In 2016 and 2018, it was rated as the world's best restaurant in the World's 50 Best Restaurants. It was the first Italian restaurant to earn the award. It was also second best in 2015 and third best in 2013 and 2014.`,
         price: 2,
       },
