93fb8398df7385238e65816c04b545996cdaf5b1
IOrozakunov23 <islamburunduk045>
2022-06-22 14:00:59 +0500

    added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from "./pages/NotFound";
 import Categories from "./pages/Categories.js";
 import Category from "./pages/Category.js";
 
+
 function App() {
   return (
     <div className="App">
