2ca6e2b2e1cce7392f6f7fb06777b09e73606d11
IOrozakunov23 <islamburunduk045>
2022-06-24 15:11:53 +0500

    add Category

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import Header from "../components/Header/Header";
 import { getCategory } from "../data/categories";
+import ProductList from "../components/CategoryList/CategoryList";
+import { getProducts } from "../data/products";
 
 function Category() {
   const params = useParams();
@@ -17,6 +19,8 @@ function Category() {
         image={category.image}>
         {category.desciption}
       </Header>
+
+      <ProductList products={getProducts(category.categoryId)} />
     </>
   );
 }
