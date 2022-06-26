1b46e16f0e56cb62cbe9bb63475a2bcea6a0689d
IOrozakunov23 <islamburunduk045>
2022-06-26 21:26:33 +0500

    store.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Home from "./pages/Home";
+import Contacts from "./pages/Contacts";
 import Products from "./pages/Products";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/layout";
 import Product from "./pages/Product";
 import NotFound from "./pages/NotFound";
-import Categories from "./pages/Categories.js";
-import Category from "./pages/Category.js";
+import Categories from "./pages/Categories";
+import Category from "./pages/Category";
 import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
@@ -18,8 +21,10 @@ function App() {
           <Route path="/products/:productId" element={<Product />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/categories/:categoryId" element={<Category />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/contacts" element={<Contacts />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/auth" element={<Auth />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
