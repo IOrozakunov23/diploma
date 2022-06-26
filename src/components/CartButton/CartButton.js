1757a32a431aec33efd3ba6e2433858e7507fcfb
IOrozakunov23 <islamburunduk045>
2022-06-27 02:13:00 +0500

    added CartButton css file

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { add } from "../../redux/cartSlice";
+import classes from "./CartButton.module.css";
 
 export default function CartButton({ productId }) {
   const dispatch = useDispatch();
