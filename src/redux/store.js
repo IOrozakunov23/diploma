82e1fc12eba942f27f770a1f3380631e2107e6f3
IOrozakunov23 <islamburunduk045>
2022-06-28 00:29:20 +0500

    chaged files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
 import cartSlice from "./cartSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
+    auth: authSlice
   }
 });
 
@@ -11,6 +13,8 @@ store.subscribe(() => {
   localStorage.setItem('cartItems', JSON.stringify(
     store.getState().cart.items
   ));
+
+  console.log(store.getState().auth);
 });
 
 export default store;
