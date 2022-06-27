3a920cbeaf77818648b85c4adede36ffbed91560
IOrozakunov23 <islamburunduk045>
2022-06-28 00:28:18 +0500

    create Auth file js

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,17 +1,41 @@
-export default function Auth() {
-  return (
-    <form>
-      <label>
-        Email:
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" />
-      </label>
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { start } from "../redux/authSlice";
 
-      <button>Sign in</button>
+export default function Auth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function onAuthStart(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    dispatch(start({
+      email: formData.get('email'),
+      password: formData.get('password'),
+      method: event.nativeEvent.submitter.innerText === "Sign up" ? 'signup' : 'signin',
+    }));
+
+    navigate('/');
+  }
+
+  return (
+    <div className="Auth">
+ <form onSubmit={onAuthStart}>
+ <div className="Auth">
+          <input type="email" placeholder="E-mail" name="email" required />
+          <input
+            type="password"
+            placeholder="Password"       
+            name="password"
+            required
+            minLength="6"
+          />
+        </div>
+<button>Sign in</button>
       <button>Sign up</button>
-    </form>
+</form>
+
+    </div>
   );
 }
