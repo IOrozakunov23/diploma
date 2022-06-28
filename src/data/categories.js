c2c5711f64413c3bdad998a714d606c3efe1d074
IOrozakunov23 <islamburunduk045>
2022-06-28 23:13:24 +0500

    added new drink's product

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,21 +1,22 @@
-import colsImage from "../assets/cola.jpg";
+import colsImage from "../assets/cols.jpg";
 import spriteImage from "../assets/sprite.jpg";
 import fantaImage from "../assets/fanta.jpg";
 import cocteilsImage from "../assets/drink.jpg";
-import cocteils1Image from "../assets/drink1.jpg";
-import cocteils2Image from "../assets/drink3.jpg";
+import moxitoImage from "../assets/moxito.jpg";
+import orangeImage from "../assets/orange.jpg";
 const categories = [
   {
     image: colsImage,
     productId: "cola",
     title: "Cola",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis arcu purus, rhoncus fringilla vestibulum vel, dignissim vel ante. Nulla facilisi. Nullam a urna sit amet tellus pellentesque egestas in in ante.`
+    description: `Osteria Francescana is a restaurant owned and run by chef Massimo Bottura in Modena, Italy. In 2018, William Reed Business Media named Osteria Francescana the best restaurant in the world that year in their annual The World's 50 Best Restaurants. In 2016 and 2018, it was rated as the world's best restaurant in the World's 50 Best Restaurants. It was the first Italian restaurant to earn the award. It was also second best in 2015 and third best in 2013 and 2014.`
   },
   {
     image: spriteImage,
     productId: "sprite",
     title: "Sprite",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis arcu purus, rhoncus fringilla vestibulum vel, dignissim vel ante. Nulla facilisi. Nullam a urna sit amet tellus pellentesque egestas in in ante.`
+    description: `Osteria Francescana is a restaurant owned and run by chef Massimo Bottura in Modena, Italy. In 2018, William Reed Business Media named Osteria Francescana the best restaurant in the world that year in their annual The World's 50 Best Restaurants. In 2016 and 2018, it was rated as the world's best restaurant in the World's 50 Best Restaurants. It was the first Italian restaurant to earn the award. It was also second best in 2015 and third best in 2013 and 2014.`,
+    price: "5$",
   },
   {
     image: fantaImage,
@@ -26,19 +27,20 @@ const categories = [
   {
   image: cocteilsImage,
     productId: "cocteil",
-    title: "cocteil",
+    title: "Cocteil",
     description: `Osteria Francescana is a restaurant owned and run by chef Massimo Bottura in Modena, Italy. In 2018, William Reed Business Media named Osteria Francescana the best restaurant in the world that year in their annual The World's 50 Best Restaurants. In 2016 and 2018, it was rated as the world's best restaurant in the World's 50 Best Restaurants. It was the first Italian restaurant to earn the award. It was also second best in 2015 and third best in 2013 and 2014.`
   },
   {
-    image: cocteils1Image,
+    image: moxitoImage,
       productId: "cocteil",
-      title: "cocteils",
-      description: `Osteria Francescana is a restaurant owned and run by chef Massimo Bottura in Modena, Italy. In 2018, William Reed Business Media named Osteria Francescana the best restaurant in the world that year in their annual The World's 50 Best Restaurants. In 2016 and 2018, it was rated as the world's best restaurant in the World's 50 Best Restaurants. It was the first Italian restaurant to earn the award. It was also second best in 2015 and third best in 2013 and 2014.`
+      title: "Moxito",
+      description: `Osteria Francescana is a restaurant owned and run by chef Massimo Bottura in Modena, Italy. In 2018, William Reed Business Media named Osteria Francescana the best restaurant in the world that year in their annual The World's 50 Best Restaurants. In 2016 and 2018, it was rated as the world's best restaurant in the World's 50 Best Restaurants. It was the first Italian restaurant to earn the award. It was also second best in 2015 and third best in 2013 and 2014.`,
+      price: "5"
     },
     {
-      image: cocteils2Image,
+      image: orangeImage,
         productId: "cocteil",
-        title: "cocteils",
+        title: "Orange juice",
         description: `Osteria Francescana is a restaurant owned and run by chef Massimo Bottura in Modena, Italy. In 2018, William Reed Business Media named Osteria Francescana the best restaurant in the world that year in their annual The World's 50 Best Restaurants. In 2016 and 2018, it was rated as the world's best restaurant in the World's 50 Best Restaurants. It was the first Italian restaurant to earn the award. It was also second best in 2015 and third best in 2013 and 2014.`
       },
 ];
