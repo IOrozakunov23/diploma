e6ca21a5e9a5d49834da0bc343dd9b6fdae354f0
IOrozakunov23 <islamburunduk045>
2022-06-29 15:43:57 +0500

    Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -5,6 +5,7 @@ function Contacts() {
   return (
     <>
       <Header
+       className="about"
         title="About"
         image={headerImage}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores explicabo, quas laudantium accusamus consequuntur ipsa, eius cumque dolorem doloribus eos dolore in illo mollitia repellat modi nam nihil aliquam qui!
